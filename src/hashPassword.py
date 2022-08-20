e6e44a318a0e9d4649f4336c44e884a53d379627
1207Lemon <meihuqin@gmail.com>
2022-08-19 23:22:36 -0400

    Update Requirements.txt

--- src/hashPassword.py
+++ src/hashPassword.py
@@ -1,8 +1,8 @@
 import streamlit_authenticator as stauth
 
-name_list = ['meihu','cheng','admin','root','team1','team3','team4','airflow']
-username_list = ['meihu','lemon','admin','root','team1','team3','team4','airflow']
-passwords = ['meihu','cheng','admin','root','team1','team3','team4','airflow']
+name_list = ['meihu','cheng','prof','ta','team1','team3','team4','airflow']
+username_list = ['meihu','lemon','prof','ta','team1','team3','team4','airflow']
+passwords = ['meihu','cheng','prof','ta','team1','team3','team4','airflow']
 
 hashed_passwords = stauth.Hasher(passwords).generate()
 
